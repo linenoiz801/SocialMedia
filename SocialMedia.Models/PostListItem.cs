e520e5d4ab321318e61ddfaf4635843777e555db
Adam Cohill <adamcohill16@gmail.com>
2021-05-01 18:18:28 -0400

    pull after 2nd merge

--- SocialMedia.Models/PostListItem.cs
+++ SocialMedia.Models/PostListItem.cs
@@ -11,7 +11,7 @@ namespace SocialMedia.Models
         public int Id { get; set; }
         public string Title { get; set; }        
         public Guid AuthorId { get; set; }
-        //public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+        public virtual List<CommentListItem> Comments { get; set; } = new List<CommentListItem>();
         //public virtual List<Like> Likes { get; set; } = new List<Like>();
     }
 }
