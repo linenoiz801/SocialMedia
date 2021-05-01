404fbdfd0319193455782fd64d6487989f7ed78f
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-01 17:56:43 -0400

    scaffolded out ReplyService

---
+++ SocialMedia.Services/ReplyService.cs
@@ -0,0 +1,54 @@
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Services
+{
+    public class ReplyService
+    {
+        private readonly Guid _userId;
+        public ReplyService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateReply(ReplyCreate model)
+        {
+            var entity =
+                new Reply()
+                {
+                    AuthorID = _userId,
+                    Text = model.Text
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Replies.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ReplyListItem> GetReplies()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Replies
+                        .Where(e => e.AuthorID == _userId)
+                        .Select(
+                            e =>
+                                new ReplyListItem
+                                {
+                                    Id = e.Id,
+                                    Text = e.Text
+                                }
+                         );
+                return query.ToArray();
+            }
+        }
+    }
+}
