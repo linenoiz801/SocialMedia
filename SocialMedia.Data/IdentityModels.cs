2e3f117559057e24c3bb69e47323361774a69aa6
Paul Gailey <linenoiz@gmail.com>
2021-04-30 21:16:55 -0600

    Added Post model

--- SocialMedia.Data/IdentityModels.cs
+++ SocialMedia.Data/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -29,5 +30,7 @@ namespace SocialMedia.Data
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Post> Posts { get; set; }
     }
 }
---
+++ SocialMedia.Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        //public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+        //public virtual List<Like> Likes { get; set; } = new List<Like>();
+    }
+}
