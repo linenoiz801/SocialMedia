df6f4f4574bfec75ea01477e7b1f95cc7b541c8f
Adam Cohill <adamcohill16@gmail.com>
2021-05-01 18:36:57 -0400

    foreign key push

--- SocialMedia.Data/Comment.cs
+++ SocialMedia.Data/Comment.cs
@@ -18,7 +18,6 @@ namespace SocialMedia.Data
         public Guid AuthorId { get; set; }
         [ForeignKey("Post")]
         public int PostId { get; set; }
-        //public virtual List<Replies> CommentReplies { get; set; }
-
+        public virtual Post Post { get; set; }
     }
 }
--- SocialMedia.Models/PostListItem.cs
+++ SocialMedia.Models/PostListItem.cs
@@ -11,7 +11,7 @@ namespace SocialMedia.Models
         public int Id { get; set; }
         public string Title { get; set; }        
         public Guid AuthorId { get; set; }
-        //public virtual List<CommentListItem> Comments { get; set; } = new List<CommentListItem>();
+        public virtual List<CommentListItem> Comments { get; set; } = new List<CommentListItem>();
         //public virtual List<Like> Likes { get; set; } = new List<Like>();
     }
 }
