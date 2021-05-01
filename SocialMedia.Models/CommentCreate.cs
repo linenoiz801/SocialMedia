c1e49dc6d6908bbc33fb684f93ff28bbda1ba51e
Adam Cohill <adamcohill16@gmail.com>
2021-05-01 16:42:31 -0400

    Added comment models

---
+++ SocialMedia.Models/CommentCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    class CommentCreate
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage ="Please Enter More Charaters.")]
+        [MaxLength (6000,ErrorMessage ="You Entered Too Many Characters.")]
+        public string CommentText { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+    }
+}
