95158a557f09c98cc470ea7f7e77fe28cf19922d
Paul Gailey <linenoiz@gmail.com>
2021-05-01 12:25:07 -0600

    WIP - Adding Post stuff to SocialMedia.Service

---
+++ SocialMedia.Services/PostService.cs
@@ -0,0 +1,36 @@
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    AuthorId = _userId,
+                    Text = model.Text,
+                    Title = model.Title
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+
+            }
+
+        }
+    }
+}
