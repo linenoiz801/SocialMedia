e67e10662e56d530d5a8534736072b71a8064a1f
Paul Gailey <linenoiz@gmail.com>
2021-05-01 15:56:38 -0600

    Wrapping up entity models

--- SocialMedia.Data/IdentityModels.cs
+++ SocialMedia.Data/IdentityModels.cs
@@ -34,6 +34,8 @@ namespace SocialMedia.Data
         }
 
         public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder
