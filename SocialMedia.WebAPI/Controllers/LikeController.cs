75579634a9ec283bc1bd6fcf617d8c7556bb5ed4
JoshuaMDenman <joshuamdenman@gmail.com>
2021-05-02 16:29:24 -0400

    Created like controller

---
+++ SocialMedia.WebAPI/Controllers/LikeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMedia.WebAPI.Controllers
+{
+    [Authorize]
+    public class LikeController : ApiController
+    {
+        private LikeService CreateLikeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var likeService = new LikeService(userId);
+            return likeService;
+        }
+        public IHttpActionResult Get(int postId)
+        {
+            LikeService likeService = CreateLikeService();
+            var likes = likeService.GetLikesByPostId(postId);
+            return Ok(likes);
+        }
+        public IHttpActionResult Post(LikeCreate like)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateLikeService();
+
+            if (!service.CreateLike(like))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
