f7549af87a474b4f067ea698a1f7e74a255c97dc
JoshuaMDenman <joshuamdenman@gmail.com>
2021-05-02 15:52:12 -0400

    Created list and create items

---
+++ SocialMedia.Models/LikeListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class LikeListItem
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
